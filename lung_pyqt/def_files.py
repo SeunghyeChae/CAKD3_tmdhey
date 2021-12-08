f0cc98701dd32daab90b3e639b77ea6a99cd4985
SeunghyeChae <77530039+SeunghyeChae@users.noreply.github.com>
2021-12-09 07:58:07 +0900

    zz

---
+++ lung_pyqt/def_files.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+def read_img(path):
+    original_img = cv2.imread(path)
+    return original_img
+
+def recall(y_target, y_pred):
+    # clip(t, clip_value_min, clip_value_max) : clip_value_min~clip_value_max 이외 가장자리를 깎아 낸다
+    # round : 반올림한다
+    y_target_yn = K.round(K.clip(y_target, 0, 1)) # 실제값을 0(Negative) 또는 1(Positive)로 설정한다
+    y_pred_yn = K.round(K.clip(y_pred, 0, 1)) # 예측값을 0(Negative) 또는 1(Positive)로 설정한다
+
+    # True Positive는 실제 값과 예측 값이 모두 1(Positive)인 경우이다
+    count_true_positive = K.sum(y_target_yn * y_pred_yn) 
+
+    # (True Positive + False Negative) = 실제 값이 1(Positive) 전체
+    count_true_positive_false_negative = K.sum(y_target_yn)
+
+    # Recall =  (True Positive) / (True Positive + False Negative)
+    # K.epsilon()는 'divide by zero error' 예방차원에서 작은 수를 더한다
+    recall = count_true_positive / (count_true_positive_false_negative + K.epsilon())
+
+    # return a single tensor value
+    return recall
+
+
+def precision(y_target, y_pred):
+    # clip(t, clip_value_min, clip_value_max) : clip_value_min~clip_value_max 이외 가장자리를 깎아 낸다
+    # round : 반올림한다
+    y_pred_yn = K.round(K.clip(y_pred, 0, 1)) # 예측값을 0(Negative) 또는 1(Positive)로 설정한다
+    y_target_yn = K.round(K.clip(y_target, 0, 1)) # 실제값을 0(Negative) 또는 1(Positive)로 설정한다
+
+    # True Positive는 실제 값과 예측 값이 모두 1(Positive)인 경우이다
+    count_true_positive = K.sum(y_target_yn * y_pred_yn) 
+
+    # (True Positive + False Positive) = 예측 값이 1(Positive) 전체
+    count_true_positive_false_positive = K.sum(y_pred_yn)
+
+    # Precision = (True Positive) / (True Positive + False Positive)
+    # K.epsilon()는 'divide by zero error' 예방차원에서 작은 수를 더한다
+    precision = count_true_positive / (count_true_positive_false_positive + K.epsilon())
+
+    # return a single tensor value
+    return precision
+
+def f1_score(y_target, y_pred):
+    _recall = recall(y_target, y_pred)
+    _precision = precision(y_target, y_pred)
+    # K.epsilon()는 'divide by zero error' 예방차원에서 작은 수를 더한다
+    _f1score = ( 2 * _recall * _precision) / (_recall + _precision+ K.epsilon())
+    
+    # return a single tensor value
+    return _f1score
+
+
+def f1_loss(y_true, y_pred):
+    
+    tp = K.sum(K.cast(y_true*y_pred, 'float'), axis=0)
+    tn = K.sum(K.cast((1-y_true)*(1-y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1-y_true)*y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true*(1-y_pred), 'float'), axis=0)
+
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
+
+    f1 = 2*p*r / (p+r+K.epsilon())
+    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
+    return 1 - K.mean(f1)
+
+
+def prep(og_img_path,size):
+    og_img = cv2.imread(og_img_path)
+    # clahe
+    clahe = cv2.createCLAHE(5.0,(8,8))
+    ## og_img_convert
+    cvt_img = cv2.resize(og_img, size, interpolation = cv2.INTER_AREA)
+    cvt_img = cv2.cvtColor(cvt_img,cv2.COLOR_BGR2GRAY)
+    cvt_img = clahe.apply(cvt_img)  # 1 channel
+    cvt_img = np.repeat(cvt_img[:,:,np.newaxis],3,-1)
+    
+    return cvt_img
+
